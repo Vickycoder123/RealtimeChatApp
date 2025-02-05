39094b2e2d79b0598b03b66619caa812db9bb154
Vickycoder123 <vikaskumarg800@gmail.com>
2025-02-05 21:34:37 +0530

    Backend setup

---
+++ Backend/chat-app-backend/chat-app-backend/src/main/java/com/vikas/chat/repositories/RoomRepository.java
@@ -0,0 +1,7 @@
+package com.vikas.chat.repositories;
+
+import com.vikas.chat.entities.Room;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface RoomRepository extends MongoRepository<Room, String> {
+}
